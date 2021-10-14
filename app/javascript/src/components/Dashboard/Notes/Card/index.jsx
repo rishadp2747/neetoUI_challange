acdd8d059deac79eba7ece6623521c301cfc3857
rishadp <rishadp2747@gmail.com>
2021-10-14 10:59:47 +0530

    updated Card

--- app/javascript/src/components/Dashboard/Notes/Card/index.jsx
+++ app/javascript/src/components/Dashboard/Notes/Card/index.jsx
@@ -6,7 +6,7 @@ import CardHeader from "./CardHeader";
 
 const Card = ({ title, children, status, showAlert, editNote }) => {
   return (
-    <div className="flex flex-col neeto-ui-border-gray-400 neeto-ui-shadow-xs border p-3 space-y-2 my-2  ">
+    <div className="flex flex-col neeto-ui-border-gray-400 neeto-ui-shadow-xs border p-3 space-y-2 my-2">
       <CardHeader title={title} showAlert={showAlert} editNote={editNote} />
       <CardBody>{children}</CardBody>
       <CardFooter status={status} />
