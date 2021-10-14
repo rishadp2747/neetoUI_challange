580c065efc089c1b9adac03a68212fd193107039
rishadp <rishadp2747@gmail.com>
2021-10-14 23:48:56 +0530

    Removed unused codes

--- app/javascript/src/components/Dashboard/Contacts/Table/index.jsx
+++ app/javascript/src/components/Dashboard/Contacts/Table/index.jsx
@@ -12,7 +12,7 @@ const Table = () => {
       {CONTACTS.map((rec, index) => (
         <TableRow
           key={index}
-          check={index}
+          check={index.toString()}
           name={rec.name}
           email={rec.email}
           createdAt={rec.created}
@@ -20,59 +20,6 @@ const Table = () => {
           profileImg={rec.profileImg}
         />
       ))}
-
-      {/*
-
-      <tbody>
-        {Array(50)
-          .fill()
-          .map((_, index) => (
-            <React.Fragment key={index}>
-              <tr>
-                <td>
-                  <Checkbox name="1" />
-                </td>
-                <td>Goutham Subramanyam</td>
-                <td>goutham.subramanyam@bigbinary.com</td>
-                <td>BigBinary</td>
-                <td>+91 9633123456</td>
-                <td>
-                  <div className="flex flex-row items-center justify-end space-x-3">
-                    <Dropdown
-                      icon={MenuHorizontal}
-                      buttonStyle="icon"
-                      autoWidth
-                    >
-                      <li>Edit</li>
-                      <li>Delete</li>
-                    </Dropdown>
-                  </div>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <Checkbox name="2" />
-                </td>
-                <td>Edwin Babu</td>
-                <td>edwin.babu@bigbinary.com</td>
-                <td>BigBinary</td>
-                <td>+91 8281331983</td>
-                <td>
-                  <div className="flex flex-row items-center justify-end space-x-3">
-                    <Dropdown
-                      icon={MenuHorizontal}
-                      buttonStyle="icon"
-                      autoWidth
-                    >
-                      <li>Edit</li>
-                      <li>Delete</li>
-                    </Dropdown>
-                  </div>
-                </td>
-              </tr>
-            </React.Fragment>
-          ))} */}
-      {/* </tbody> */}
     </table>
   );
 };
